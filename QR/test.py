5ed8f7d987c95ff59196b73ac579ea9998b1e27b
ngdghnam <stghoainam4002@gmail.com>
2024-09-14 23:55:53 +0700

    test AprilTag

--- QR/test.py
+++ QR/test.py
@@ -2,5 +2,6 @@ import YanAPI
 
 ip_adr = "192.168.0.160"
 YanAPI.yan_api_init(ip_adr)
-res = YanAPI.get_aprilTag_recognition_status()
+# res = YanAPI.get_aprilTag_recognition_status()
+res = YanAPI.start_aprilTag_recognition()
 print(res)
