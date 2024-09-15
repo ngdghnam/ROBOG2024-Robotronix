9277aeefcbd222534afbb6a3a0be64572bfb0422
Nguyin <nguyenlm22416c@st.uel.edu.vn>
2024-09-15 12:34:50 +0700

    Gemini test

--- Gemini_CURL.py
+++ Gemini_CURL.py
@@ -1,22 +1,44 @@
-import os
 import requests
+import json
 
-headers = {
-    'Content-Type': 'application/json',
-}
+def Gemini(user_input:str) -> str:
 
-params = {
-    'key': os.getenv('AIzaSyBbW-oad2I-g5k4pAI9K0PSjZhqqHB6QYU', ''),
-}
+    url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyBbW-oad2I-g5k4pAI9K0PSjZhqqHB6QYU'
 
-data = '{"contents": [{"role":"user","parts":[{"text": "Hello"}]},{"role": "model","parts":[{"text": "Great to meet you. What would you like to know?"}]},{"role":"user","parts":[{"text": "I have two dogs in my house. How many paws are in my house?"}]},]}'
+    headers = {
+        'Content-Type': 'application/json',
+    }
 
+    json_data = {
+        'system_instruction': 
+        {
+                'parts': 
+                    {
+                        'text': 'Answer like human speech'
+                    }
+        },
+        'contents': [
+            {
+                'parts': [
+                    {
+                        'text': user_input
+                    }
+                ]
+            }
+        ]
+    }
 
-response = requests.post(
-    'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyBbW-oad2I-g5k4pAI9K0PSjZhqqHB6QYU',
-    params=params,
-    headers=headers,
-    data=data,
-)
+    response = requests.post(
+        url,
+        headers=headers,
+        json=json_data,
+    )
 
-print(response)
+    json_file = json.loads(response.text)
+
+    answer = json_file['candidates'][0]['content']['parts'][0]['text']
+
+    return answer
+
+text = input('Enter your text: ')
+print(Gemini(text))
